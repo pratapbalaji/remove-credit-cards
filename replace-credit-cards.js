4c90114b362fe579703ef0c3d15974681563b877
Vinay Balaji <vinaybalaji@gmail.com>
2018-10-15 07:22:59 -0700

    added code comments to explain function and assumption

--- replace-credit-cards.js
+++ replace-credit-cards.js
@@ -1,3 +1,6 @@
 const removeCreditCards = module.exports = function (message) {
+  // function that replaces credit card numbers from a given message with '- ' and returns the updated message
+  // using a regular expression to check for all instances of 4 sets of 4 digit blocks
+  // for simplicity, assuming that all credit cards have 4 sets of 4 digit blocks, i.e. 1111 1111 1111 1111
   return message.toString().replace(/(?:\d{4}[ -]?){4}/g, '- ')
 }
